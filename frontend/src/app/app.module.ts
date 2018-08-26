a64914033817d1c8019017006a6eb5d4efa8821c
Simon King <simon.king@stronans.com>
2018-08-26 14:20:57 +0100

    Removed some unused components and cleaned up the CSS presentation.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 
 import {DomoticsComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
-import {DisplayStationComponent} from './DisplayStation/display-station.component';
 import {DisplayAllStations} from './DisplayAllStations/display-all-stations.component';
 import {OptionSelectComponent} from './OptionSelect/option-select.component';
 import {AppRoutingModule} from './app-routing.module';
@@ -21,20 +20,17 @@ import {DataStationService} from "./services/station.services";
 import {Configuration} from "./models/configuration";
 import {WarningComponent} from './DisplayAllStations/components/warning/warning.component';
 import {LinkToComponent} from './DisplayAllStations/components/link-to/link-to.component';
-import {DegreesComponent} from './DisplayAllStations/components/degrees/degrees.component';
 import {SensorCacheService} from "./services/sensorCache.services";
 
 @NgModule({
   declarations: [
     DomoticsComponent,
     DisplayAllStations,
-    DisplayStationComponent,
     OptionSelectComponent,
     DisplayChartComponent,
     PageNotFoundComponent,
     WarningComponent,
-    LinkToComponent,
-    DegreesComponent
+    LinkToComponent
   ],
   imports: [
     BrowserModule,
